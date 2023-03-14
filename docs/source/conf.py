21ae26d48a9d9dd462a0f0ba6275bdd9e0f53211
Gavin Medley <gavin.medley@lasp.colorado.edu>
2023-03-14 13:53:41 -0600

    Add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,4 +36,5 @@ source_suffix = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_static_path = ["_static"]
+html_logo = "_static/logo-no-background.png"
